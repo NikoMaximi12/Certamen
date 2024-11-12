949b55f3534d09373ac99ddaad6d898cb45a2c88
NikoMaximi12 <n.lincopil@alumnos.santotomas.cl>
2024-11-12 14:38:52 -0300

    Interfaz de usuario

--- app/src/main/java/com/example/certamen/MainActivity.java
+++ app/src/main/java/com/example/certamen/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.certamen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
